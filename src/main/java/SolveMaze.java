28fd567d74243925996831a423ec151609d1b17f
cyc2000 <yc16@illinois.edu>
2018-09-13 05:29:26 +0800

    Move with wall is useful

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,13 +34,37 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            if (!maze.isFinished()) {
+                maze.turnLeft();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnRight();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnRight();
+                        if (maze.canMove()) {
+                            maze.move();
+                        } else {
+                            maze.turnRight();
+                            if (maze.canMove()) {
+                                maze.move();
+                            }
+                        }
+                    }
+
+                }
+
+                // Implement your maze solving algorithm here
+            }
         }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
         }
     }
-}
+
